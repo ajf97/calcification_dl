2d2d37a805af3ecfdb56d05a5957c4f7fb8ae7d2
Alejandro Jerónimo Fuentes <ajf97@correo.ugr.es>
2022-10-16 13:16:29 +0200

    feat: add base model

---
+++ app/segmentation_app.py
@@ -0,0 +1,90 @@
+import os
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import streamlit as st
+import torch
+import torchvision.transforms as T
+from icecream import ic
+from loguru import logger
+from PIL import Image
+
+import models.unet_base as unet_base
+import preprocessing.image_transforms as imgt
+import preprocessing.target_transforms as tgt
+
+st.header("Segmentación de calcificaciones en mamografías")
+st.write(
+    "Elige la imagen de una mamografía para obtener el correspondiente mapa de segmentación."
+)
+
+uploaded_file = st.file_uploader("Sube la imagen")
+
+
+def make_prediction(img, model, postprocess, device):
+    model.eval()
+    img = img.type(torch.FloatTensor)
+    img = img[None, ...]
+    x = img.to(device)  # to torch, send to device
+
+    with torch.no_grad():
+        out = model(x)  # send through model/network
+
+    out_softmax = torch.softmax(out, dim=1)  # perform softmax on outputs
+    result = postprocess(out_softmax)  # postprocess outputs
+
+    return result
+
+
+def predict(image):
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+
+    model = unet_base.UNet(
+        in_channels=1,
+        out_channels=2,
+        n_blocks=4,
+        start_filters=32,
+        activation="relu",
+        normalization="batch",
+        conv_mode="same",
+        dim=2,
+    ).to(device)
+
+    model_path = "../src/models/cbis_ddsm_unet_base.pt"
+    torch.load(model_path, map_location=device)
+
+    return make_prediction(image, model, tgt.PostProcess(), device)
+
+
+def apply_mask(img, mask):
+    red = np.array([255, 0, 0], dtype=np.uint8)
+    img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+
+    masked_img = np.where(mask[..., None], red, img)
+    output = cv2.addWeighted(img, 0.8, masked_img, 0.2, 0)
+
+    return output
+
+
+if uploaded_file is not None:
+    # Load image and convert to numpy array
+    image = Image.open(uploaded_file)
+    st.image(uploaded_file, caption="Imagen de entrada", use_column_width=True)
+
+    image_array = np.array(image)
+
+    # Preprocess image
+    image_preprocessing_pipeline = T.Compose([imgt.NormalizeMinMax(), T.ToTensor()])
+    img_preprocessed = image_preprocessing_pipeline(image_array)
+
+    # Make prediction and display segmentation map
+    segmentation_map = predict(img_preprocessed)
+    st.image(segmentation_map, caption="Mapa de segmentación", use_column_width=True)
+
+    # Apply mask to image
+    masked_image = apply_mask(image_array, segmentation_map)
+    st.image(masked_image, caption="Calcificaciones detectadas", use_column_width=True)
---
+++ src/data/cbis_ddsm_prep.py
@@ -0,0 +1,36 @@
+import os
+import re
+
+import cv2
+import numpy as np
+from torch.utils.data import Dataset
+
+
+class CbisDatasetPreprocessed(Dataset):
+    def __init__(self, img_dir: str, transform=None, target_transform=None):
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        files_list = os.listdir(self.img_dir)
+        full_mammograms = filter(lambda file: "FULL" in file, files_list)
+        return len(list(full_mammograms))
+
+    def __getitem__(self, idx):
+        file_list = os.listdir(self.img_dir)
+        full_mammograms = list(filter(lambda file: "FULL" in file, file_list))
+        file_name = full_mammograms[idx]
+
+        img_path = os.path.join(self.img_dir, file_name)
+        image = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE).astype(np.int32)
+
+        label_path = "_".join(file_name.split("_")[:-1]) + "_MASK.png"
+        label = cv2.imread(os.path.join(self.img_dir, label_path), cv2.IMREAD_GRAYSCALE)
+
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+
+        return image, label
