30d9a55446deb82cfe1c70458ce28dba759efc51
Alejandro Jerónimo Fuentes <ajf97@correo.ugr.es>
2023-02-01 13:22:33 +0100

    feat: add a new preprocessing
    New preprocessing methods were added to be compatible with the new

---
+++ src/preprocessing/transforms.py
@@ -0,0 +1,81 @@
+import cv2
+import numpy as np
+
+
+def normalize(image: np.ndarray, mask: np.ndarray, type=np.float32):
+    image_norm = image.astype(type) / 255
+    mask_norm = mask / 255.0
+
+    return image_norm, mask_norm
+
+
+def left_mamm(image: np.ndarray, mask):
+    if image[:, :200, ...].sum() < image[:, -200:, ...].sum():
+        image[:, :, ...] = image[:, ::-1, ...]
+        mask[:, :, ...] = mask[:, ::-1, ...]
+
+    return image, mask
+
+
+def clean_mamm(image: np.ndarray) -> np.ndarray:
+    background_val = 0
+    image[:10, :, ...] = 0
+    image[-10:, :, ...] = 0
+    image[:, -10:, ...] = 0
+
+    msk1 = (image[:, :, 0] == image[:, :, 1]) & (image[:, :, 1] == image[:, :, 2])
+    image = image.mean(axis=2) * msk1
+    msk = np.uint8((image > background_val) * 255)
+    msk = cv2.morphologyEx(
+        msk, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_RECT, (50, 50))
+    )
+
+    comps = cv2.connectedComponentsWithStats(msk)
+
+    common_label = np.argmax(comps[2][1:, cv2.CC_STAT_AREA]) + 1
+
+    msk = (comps[1] == common_label).astype(np.uint8)
+
+    image[:, :] = msk * image[:, :]
+
+    return image
+
+
+def cut_mamm(image: np.ndarray, mask: np.ndarray) -> np.ndarray:
+    """Cut mamogram with an optimal width.
+
+    We remove uneccesary background which may cause a loss of essential information
+
+    """
+    act_w = get_act_width(image)
+    h = image.shape[0]
+    image = image[:h, :act_w]
+    mask = mask[:h, :act_w]
+
+    return image, mask
+
+
+def get_act_width(image: np.ndarray):
+    """This method obtains the size of width to cut
+
+    Args:
+        image (np.ndarray): input image
+
+    Returns:
+        w: size of width
+    """
+    w = image.shape[1] // 3
+
+    while image[:, w:].max() > 0:
+        w += 1
+
+    return w
+
+
+def preprocess(image: np.ndarray, mask: np.ndarray):
+    image, mask = normalize(image, mask)
+    image, mask = left_mamm(image, mask)
+    image = clean_mamm(image)
+    image, mask = cut_mamm(image, mask)
+
+    return image, mask
