c7b3938ae58ae4ce48546cb6b268fc8284a44d70
Alejandro Jerónimo Fuentes <ajf97@correo.ugr.es>
2023-02-06 16:29:28 +0100

    feat: add apply_mask function
    This function applies a segmentation map to an image.  Calcifications

--- src/train/utils.py
+++ src/train/utils.py
@@ -39,3 +39,13 @@ def segmentation_map(prediction, threshold=0.5):
     seg_map[seg_map > 0] = 1
 
     return seg_map * 255
+
+
+def apply_mask(img, mask):
+    red = np.array([255, 0, 0], dtype=np.uint8)
+    img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+
+    masked_img = np.where(mask[..., None], red, img)
+    output = cv2.addWeighted(img, 0.8, masked_img, 0.6, 0)
+
+    return output
