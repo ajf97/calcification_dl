9a79b5ae876c01756bd968522f57d3ae93b44c6b
Alejandro Jerónimo Fuentes <ajf97@correo.ugr.es>
2023-02-08 16:05:23 +0100

    style(app): add some improvements to GUI
    Width and threshold parameters were added. Now output and segmentation

--- app/segmentation_app.py
+++ app/segmentation_app.py
@@ -12,32 +12,55 @@ sys.path.insert(0, cfg.src_path)
 
 from models.fcn import initialize_model, predict
 from preprocessing import transforms
-from train.utils import apply_mask, segmentation_map
+from utils import apply_mask, segmentation_map
 
-st.header("Segmenting calcifications on mammograms")
-st.write("Choose the input image to get the segmentation map")
+favicon = Image.open(cfg.favicon)
 
-uploaded_file = st.file_uploader("Upload image")
+st.set_page_config(
+    page_title="Calc Detector",
+    page_icon=favicon,
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items=None,
+)
+
+with st.sidebar:
+    st.header("Segmenting calcifications on mammograms")
+
+    width = st.number_input("Image resolution (width)", min_value=10, max_value=4000)
+    threshold = st.number_input(
+        "Prediction threshold", min_value=0.0, max_value=1.0, format="%f"
+    )
+
+    st.write("Choose the input image to get the segmentation map")
+    uploaded_file = st.file_uploader("Upload image")
+    bt = st.button("Predict")
 
 
-if uploaded_file is not None:
+if bt and uploaded_file is not None:
     # Load image and convert to numpy array
     image = Image.open(uploaded_file)
-    st.image(uploaded_file, caption="Input image", use_column_width=True)
-
     image_array = np.array(image)
     image_array = cv2.cvtColor(image_array, cv2.COLOR_GRAY2RGB)
 
     # Preprocess image
-    image_preprocessed = transforms.preprocess(image_array, width=1500)
+    image_preprocessed = transforms.preprocess(image_array, width=width)
 
     # Load the model and make the prediction
     model = initialize_model(cfg.kios_model)
     pred = predict(model, image_preprocessed)
-    sm = segmentation_map(pred, threshold=0.001)
-
-    st.image(sm, caption="Segmentation map", use_column_width=True, clamp=True)
+    sm = segmentation_map(pred, threshold=threshold)
 
     # Apply mask to image
     masked_image = apply_mask(image_preprocessed, sm)
-    st.image(masked_image, caption="output", use_column_width=True, clamp=True)
+
+    # Put the images in two columns
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.header("Output")
+        st.image(masked_image, use_column_width=True, clamp=True)
+
+    with col2:
+        st.header("Segmentation map")
+        st.image(sm, use_column_width=True, clamp=True)
