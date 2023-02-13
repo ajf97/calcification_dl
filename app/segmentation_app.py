165a0abe75b1b83f17727e77ae6430807d2eea49
Alejandro Jerónimo Fuentes <ajf97@correo.ugr.es>
2023-02-13 15:17:23 +0100

    feat(app): add output and probability heatmap into tabs
    In this commit, two tabs were added to the web app. Now, you can display

--- app/segmentation_app.py
+++ app/segmentation_app.py
@@ -1,6 +1,8 @@
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
 import omegaconf
+import plotly.express as px
 import streamlit as st
 from PIL import Image
 
@@ -19,7 +21,7 @@ favicon = Image.open(cfg.favicon)
 st.set_page_config(
     page_title="Calc Detector",
     page_icon=favicon,
-    layout="centered",
+    layout="wide",
     initial_sidebar_state="auto",
     menu_items=None,
 )
@@ -27,13 +29,14 @@ st.set_page_config(
 with st.sidebar:
     st.header("Segmenting calcifications on mammograms")
 
-    width = st.number_input("Image resolution (width)", min_value=10, max_value=4000)
+    width = st.number_input(
+        "Image resolution (width)", value=1000, min_value=10, max_value=4000
+    )
     threshold = st.number_input(
-        "Prediction threshold", min_value=0.0, max_value=1.0, format="%f"
+        "Prediction threshold", value=0.01, min_value=0.0, max_value=1.0, format="%f"
     )
 
-    st.write("Choose the input image to get the segmentation map")
-    uploaded_file = st.file_uploader("Upload image")
+    uploaded_file = st.file_uploader("Upload the input image")
     bt = st.button("Predict")
 
 
@@ -49,18 +52,35 @@ if bt and uploaded_file is not None:
     # Load the model and make the prediction
     model = initialize_model(cfg.kios_model)
     pred = predict(model, image_preprocessed)
+    predi = pred.copy()
     sm = segmentation_map(pred, threshold=threshold)
 
     # Apply mask to image
     masked_image = apply_mask(image_preprocessed, sm)
 
     # Put the images in two columns
-    col1, col2 = st.columns(2)
+    tab1, tab2 = st.tabs(["📈 Output and probability heatmap", "🎭 Segmentation mask"])
+    col1, col2 = st.columns(2, gap="large")
 
-    with col1:
-        st.header("Output")
-        st.image(masked_image, use_column_width=True, clamp=True)
+    with tab1:
+        with col1:
+            # st.header("Output")
+            fig = plt.imshow(masked_image)
+            pr = fig.make_image(renderer=None, unsampled=True)[0]
+            fig = px.imshow(pr, aspect="equal")
+            fig.update_layout(width=600, height=600)
+            col1.plotly_chart(fig, use_container_width=True)
 
-    with col2:
-        st.header("Segmentation map")
-        st.image(sm, use_column_width=True, clamp=True)
+        with col2:
+            # st.header("Probability heatmap")
+            fig = px.imshow(predi, aspect="equal", color_continuous_scale="turbo")
+            fig.update_layout(width=600, height=600)
+            col2.plotly_chart(fig, use_container_width=True)
+
+    with tab2:
+        # st.header("Segmentation map")
+        fig = px.imshow(sm, aspect="equal", color_continuous_scale="gray")
+        fig.update(layout_coloraxis_showscale=False)
+        fig.update_layout(width=600, height=600)
+        st.plotly_chart(fig, use_column_width=True, clamp=True)
+
